797e95d98502a2e31ae1a090dcbda67d20c3dc21
peter <pb@calldorado.com>
2021-11-10 11:16:51 +0100

    Created onActivity callbacks and utility functions for days and time

--- preonboarding/src/main/java/com/calldorado/preonboarding/UpdateActivity.kt
+++ preonboarding/src/main/java/com/calldorado/preonboarding/UpdateActivity.kt
@@ -1,11 +1,13 @@
 package com.calldorado.preonboarding
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import com.calldorado.preonboarding.databinding.ActivityUpdateBinding
 import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.install.model.ActivityResult.RESULT_IN_APP_UPDATE_FAILED
 
 class UpdateActivity : AppCompatActivity() {
 
@@ -23,9 +25,40 @@ class UpdateActivity : AppCompatActivity() {
         PreonboardingApi.updateImmediately(this) { statusOnUpdate ->
             binding.displayTxt.text = statusOnUpdate.toString()
         }
-        binding.buttonExit.setOnClickListener({
+        binding.buttonExit.setOnClickListener {
             finish()
-        })
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PreonboardingApi.UPDATE_REQUEST_CODE -> {
+                when (resultCode){
+                    //The user has accepted the update. For immediate updates, you might not receive
+                    // this callback because the update should already be finished by the time control
+                    // is given back to your app.
+                    RESULT_OK -> {
+                        val intent = packageManager.getLaunchIntentForPackage(packageName)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                    //The user has denied or canceled the update.
+                    RESULT_CANCELED -> {
+                        //TODO set notification timer as spec'ed
+                    }
+
+                    //Some other error prevented either the user from providing consent or the update from proceeding.
+                    RESULT_IN_APP_UPDATE_FAILED -> {
+                        //TODO set notification timer as spec'ed
+                    }
+
+                    else -> {
+                        //TODO set notification timer as spec'ed
+                    }
+                }
+            }
+        }
+    }
 }
--- preonboarding/src/main/java/com/calldorado/preonboarding/Utils.kt
+++ preonboarding/src/main/java/com/calldorado/preonboarding/Utils.kt
@@ -2,20 +2,37 @@ package com.calldorado.preonboarding
 
 import android.content.Context
 import android.content.pm.PackageManager
+import java.util.*
 
 class Utils {
-    companion object{
+    companion object {
         fun getMetadataLaunchClass(context: Context): String? {
             context.packageManager.getApplicationInfo(
                 context.packageName,
                 PackageManager.GET_META_DATA
             ).apply {
                 metaData?.let {
-                    val launchClass = metaData.getString("com.calldorado.preonboarding.launch_class")
+                    val launchClass =
+                        metaData.getString("com.calldorado.preonboarding.launch_class")
                     return launchClass
                 }
                 return null
             }
+        }
+
+        fun isSunday(): Boolean {
+            val calendar: Calendar = Calendar.getInstance()
+            val day: Int = calendar.get(Calendar.DAY_OF_WEEK)
+
+            return when (day) {
+                Calendar.SUNDAY -> true
+                else -> false
+            }
+        }
+
+        fun isWithinTimeframe(): Boolean {
+            val currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            return currentTime in 15..18
         }
     }
 }
